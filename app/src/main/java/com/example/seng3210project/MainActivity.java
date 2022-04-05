2a0888d441b69c1972f091b4a177b04c0b773a48
gearlock <sanyamg37@gmail.com>
2022-04-05 14:54:46 -0700

    Finall got Main activity intents working going to finish soon

--- app/src/main/java/com/example/seng3210project/MainActivity.java
+++ app/src/main/java/com/example/seng3210project/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.seng3210project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public static void GotoVoterRegistration(){
-
-
-
+    public void IntentVoterRegistration(View view){
+        startActivity(new Intent(getApplicationContext(),registration_screen_voter.class));
     }
+    public void IntentAdminRegistration(View view){
+        startActivity(new Intent(getApplicationContext(),registration_screen_admin.class));
+    }
+    public void IntentLoginScreen(View view){
+        startActivity(new Intent(getApplicationContext(),login_screen.class));
+    }
+
 }
---
+++ app/src/main/java/com/example/seng3210project/registration_screen_admin.java
@@ -0,0 +1,14 @@
+package com.example.seng3210project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration_screen_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_screen_admin);
+    }
+}
