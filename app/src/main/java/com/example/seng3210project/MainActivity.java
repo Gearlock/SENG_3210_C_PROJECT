17ea04f9eceab9022b5d7ba28b3ab5d87e1cf1eb
gearlock <sanyamg37@gmail.com>
2022-04-04 21:27:50 -0700

    tried implementing intents, still working on it, but did it for main java"

--- app/src/main/java/com/example/seng3210project/MainActivity.java
+++ app/src/main/java/com/example/seng3210project/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.seng3210project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,13 +12,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void IntentVoterRegistration(View view){
-        startActivity(new Intent(getApplicationContext(),registration_screen_voter.class));
-    }
-    public void IntentAdminRegistration(View view){
-        startActivity(new Intent(getApplicationContext(),registration_screen_admin.class));
-    }
-    public void IntentLoginScreen(View view){
-        startActivity(new Intent(getApplicationContext(),login_screen.class));
+    public static void GotoVoterRegistration(){
+
+
+
     }
 }
