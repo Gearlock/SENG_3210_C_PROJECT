45493e80dbcf558a7757e4ed1a8fc4b174b81632
gearlock <sanyamg37@gmail.com>
2022-04-04 21:22:40 -0700

    tried implementing intents, still working on it, but did it for main java

--- app/src/main/java/com/example/seng3210project/MainActivity.java
+++ app/src/main/java/com/example/seng3210project/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.seng3210project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public static void GotoVoterRegistration(){
-
-
-
+    public void IntentVoterRegistration(View view){
+        startActivity(new Intent(getApplicationContext(),registration_screen_voter.class));
+    }
+    public void IntentAdminRegistration(View view){
+        startActivity(new Intent(getApplicationContext(),registration_screen_admin.class));
+    }
+    public void IntentLoginScreen(View view){
+        startActivity(new Intent(getApplicationContext(),login_screen.class));
     }
 }
---
+++ app/src/main/java/com/example/seng3210project/registration_screen_admin.java
@@ -0,0 +1,13 @@
+package com.example.seng3210project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class registration_screen_admin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_screen_admin);
+    }
+}
