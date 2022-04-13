d66537e578524a54ac9229d55b4b6937ddbb8ba5
gearlock <sanyamg37@gmail.com>
2022-04-12 21:23:36 -0700

    added more empty activities, populating them soon

---
+++ app/src/main/java/com/example/seng3210project/review_voter_details.java
@@ -0,0 +1,14 @@
+package com.example.seng3210project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class review_voter_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.review_voter_details);
+    }
+}
--- app/src/main/java/com/example/seng3210project/voterMainPage.java
+++ app/src/main/java/com/example/seng3210project/voterMainPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class voterMainPage extends AppCompatActivity {
 
@@ -13,5 +14,13 @@ public class voterMainPage extends AppCompatActivity {
         setContentView(R.layout.voter_main_page);
     }
 
-    //startActivity(new Intent(getApplicationContext(),registration_screen_voter.class));
+    public void IntentVoterResults(View view) {
+        startActivity(new Intent(getApplicationContext(),voter_voting_results_page.class));
+    }
+
+    public void IntentVotingForTopic(View view) {
+        startActivity(new Intent(getApplicationContext(),voteForTopic.class));
+    }
+
+
 }
