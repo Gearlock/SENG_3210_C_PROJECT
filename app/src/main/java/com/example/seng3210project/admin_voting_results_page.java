60b158785010324ed2b80fe55b3ae20cb6dbd49e
dbehnke38 <domenickb@hotmail.com>
2022-04-13 13:55:01 -0700

    Fixed intents, added "back to menu" button for the Vote for Topic screen

--- app/src/main/java/com/example/seng3210project/admin_voting_results_page.java
+++ app/src/main/java/com/example/seng3210project/admin_voting_results_page.java
@@ -2,7 +2,9 @@ package com.example.seng3210project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class admin_voting_results_page extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class admin_voting_results_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_voting_results_page);
+    }
+
+    public void IntentAdminMainPage(View view) {
+        startActivity(new Intent(getApplicationContext(),manager_main_page.class));
     }
 }
--- app/src/main/java/com/example/seng3210project/voteForTopic.java
+++ app/src/main/java/com/example/seng3210project/voteForTopic.java
@@ -2,7 +2,9 @@ package com.example.seng3210project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class voteForTopic extends AppCompatActivity { // Function to to allow a voter to vote for or against a topic
 
@@ -10,5 +12,9 @@ public class voteForTopic extends AppCompatActivity { // Function to to allow a 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vote_for_topic);
+    }
+
+    public void IntentAdminMainPage(View view) {
+        startActivity(new Intent(getApplicationContext(),manager_main_page.class));
     }
 }
