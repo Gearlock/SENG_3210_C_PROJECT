f9266bb0e24d462c848f2a0aec58e6e2264d6c7f
gearlock <sanyamg37@gmail.com>
2022-04-12 21:38:34 -0700

    add topic to database done

---
+++ app/src/main/java/com/example/seng3210project/Topic.java
@@ -0,0 +1,14 @@
+package com.example.seng3210project;
+
+public class Topic {
+    String topicName,startDate,endDate;
+    public Topic(){
+
+    }
+    public Topic(String topicName,String startDate,String endDate){
+        this.topicName = topicName;
+        this.endDate = endDate;
+        this.startDate = startDate;
+    }
+
+}
